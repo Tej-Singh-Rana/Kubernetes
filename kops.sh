c1e81e4e7b1faf77c0269ce4973a46d13e386834
root <root@ip-172-31-43-203.ap-south-1.compute.internal>
2020-03-07 12:36:13 +0000

    kops process

---
+++ kops.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+
+chmod +x kops-linux-amd64
+
+sudo mv kops-linux-amd64 /usr/bin/kops
+sleep 5
+echo "source <(kops completion bash)" >> /root/.bashrc
+source /root/.bashrc
